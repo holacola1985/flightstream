774b5b570bb9d38f07556ab385e04cdae6f4538b
Franck Ernewein <franck.ernewein@gmail.com>
2015-07-21 11:52:15 +0200

    add mapbox map

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,16 @@
-console.log('yolol');
+require('mapbox.js');
+
+var L = window.L;
+
+L.mapbox.accessToken = 'pk.eyJ1IjoiZnJhbmNrZXJuZXdlaW4iLCJhIjoiYXJLM0dISSJ9.mod0ppb2kjzuMy8j1pl0Bw';
+
+
+var map = L.mapbox.map('map', 'franckernewein.70945bd6', {
+  zoomControl: false,
+  attributionControl: false,
+  tileLayer: {
+    noWrap: true
+  }
+});
+
+//map.setView([0, 0]);
