d92d1e652b8bf99f6e3ff4af1a2a575b9a139393
Franck Ernewein <franck.ernewein@gmail.com>
2015-07-22 10:27:51 +0200

    allow to rotate map

--- src/js/Control3D.js
+++ src/js/Control3D.js
@@ -6,28 +6,41 @@ function Control3D(options) {
   this.$map = $(this.map._container);
   this.el = options.el;
   this.$el = $(this.el);
+  this.$button = $('button', this.$el);
+  this.$input = $('input', this.$el);
   this.isIn3d = false;
-  this.$el.click(this.toggle.bind(this));
+  this.$button.click(this.toggle.bind(this));
+  this.$input.on('change', function() {
+    this.rotate(this.$input.val());
+  }.bind(this));
 }
 
 Control3D.prototype.set3dMode = function set3dMode() {
+  this.$el.addClass('on');
   this.$map.addClass('pers');
+  this.$map.css('transform', 'translateY(50px) rotateX(70deg)');
   this.isIn3d = true;
 };
 
 Control3D.prototype.setNormalMode = function setNormalMode() {
+  this.$el.removeClass('on');
   this.$map.removeClass('pers');
+  this.$map.css('transform', 'translateY(0px) rotateX(0deg)');
   this.isIn3d = false;
 };
 
 Control3D.prototype.toggle = function toggle(e) {
   e.preventDefault();
-  if(this.isIn3d){
-    this.setNormalMode(); 
-  }else{
+  if (this.isIn3d) {
+    this.setNormalMode();
+  } else {
     this.set3dMode();
   }
 };
+
+Control3D.prototype.rotate = function rotate(angle) {
+  this.$map.css('transform', 'translateY(50px) rotateX(70deg) rotateZ(' + angle + 'deg)');
+}
 
 
 module.exports = Control3D;
--- src/js/index.js
+++ src/js/index.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
 
   new Control3D({
     map: map,
-    el: $('.control-3d')
+    el: $('.control-3d'),
   });
 
   //control.set3dMode();
@@ -29,13 +29,24 @@ $(document).ready(function() {
 
   map.setView([1.367245, 103.809247], 17);
 
+  var getPxBounds = map.getPixelBounds;
+  map.getPixelBounds = function() {
+    var bounds = getPxBounds.call(this);
+    // ... extend the bounds
+    bounds.min.x = bounds.min.x - 2000;
+    bounds.min.y = bounds.min.y - 2000;
+    bounds.max.x = bounds.max.x + 2000;
+    bounds.max.y = bounds.max.y + 2000;
+    return bounds;
+  };
+
   var layer = new DroneLayer();
   map.addLayer(layer);
-  layer.appendMarker([1.367245, 103.809247]);
-  layer.appendMarker([1.366628, 103.811908]);
   layer.appendMarker([1.365052, 103.806350]);
-  layer.appendMarker([1.367095, 103.808750]);
   layer.appendMarker([1.366775, 103.808111]);
+  layer.appendMarker([1.367245, 103.809247]);
+  layer.appendMarker([1.367095, 103.808750]);
   layer.appendMarker([1.366113, 103.807654]);
   layer.appendMarker([1.367095, 103.810338]);
+  layer.appendMarker([1.366628, 103.811908]);
 });
